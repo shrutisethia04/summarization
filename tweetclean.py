19fdbbcf854f8258372c8e57cd6b6d13d95d2fd5
shrutisethia04 <shrutisethia04@gmail.com>
2016-11-20 14:21:01 +0530

    updated

--- tweetclean.py
+++ tweetclean.py
@@ -14,22 +14,32 @@ cursor = db.cursor()
 
 slang_dictionary = {}
 
-cursor.execute("select sno, tweet from twitter;") # ctweet has punctuations,stopwords,hyperlinks removed
+tablename = 'twitter'
+
+cursor.execute("select sno, tweet from %s;" % (tablename)) # ctweet has punctuations,stopwords,hyperlinks removed
 db.commit()
 tweetset=cursor.fetchall()
 
 for tweet in tweetset:
+	up_tweet=tweet[1] #tweet original with spaces removed
+	up_tweet = re.sub(' +',' ',up_tweet).strip()
+	up_tweet = re.sub('&amp;','&',up_tweet)
+	up_tweet=up_tweet.replace('\n',"")
+	up_weet=up_tweet.replace('\r',"")
+
 	comp_tweet = tweet[1]
 	# lowercase
 	comp_tweet = comp_tweet.lower()
 	# remove links
 	comp_tweet = re.sub(r"(?:\@|https?\://)\S+", "", comp_tweet)
-	
-
+	comp_tweet = re.sub('&amp;','&',comp_tweet)
+	#incomplete tweet
+	comp_tweet = re.sub('htt','',comp_tweet)
+	comp_tweet = re.sub('http;','',comp_tweet)
+	comp_tweet = re.sub('https;','',comp_tweet)
 	# punctuation
 	exclude = set(string.punctuation)
 	comp_tweet = ''.join(ch for ch in comp_tweet if ch not in exclude)
-
 	# remove slangs
 	csvfile= open('Slang_Dictionary.csv','r')
 	reader = csv.reader(csvfile)
@@ -43,6 +53,10 @@ for tweet in tweetset:
 	comp_tweet=comp_tweet.replace('"',"")
 	comp_tweet=comp_tweet.replace('rt',"")
 	comp_tweet=comp_tweet.replace('RT',"")
+	comp_tweet=comp_tweet.replace('\n',"")
+	comp_tweet=comp_tweet.replace('\r',"")
 	comp_tweet = re.sub(' +',' ',comp_tweet).strip()
-	cursor.execute("update twitter set ctweet=%s where sno=%s;",(comp_tweet, tweet[0])) # ctweet has punctuations,stopwords,hyperlinks removed
+
+	print tweet[0]
+	cursor.execute("""update %s set tweet= "%s", ctweet='%s' where sno=%s;""" % (tablename, up_tweet, comp_tweet , `str(tweet[0])`))
 	db.commit()
